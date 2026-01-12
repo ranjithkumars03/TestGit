7bd6df90e062009bcec51d56a41ceb53eccbed70
ranji <ranji@LAPTOP-D7GRFL3O>
2026-01-12 07:47:19 +0530

    Junior2

---
+++ Demoproject/src/test/java/Github/Junior2.java
@@ -0,0 +1,10 @@
+package Github;
+
+public class Junior2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the Junior2 program");
+	}
+
+}
