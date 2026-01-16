de58335f12bed15f697ca7081e975e76f5de6686
ranji <ranji@LAPTOP-D7GRFL3O>
2026-01-16 08:53:58 +0530

    Added new class NewClassPollSCM

---
+++ Demoproject/src/test/java/Github/NewClassPollSCM.java
@@ -0,0 +1,10 @@
+package Github;
+
+public class NewClassPollSCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  System.out.println("To Test Poll SCM");
+	}
+
+}
